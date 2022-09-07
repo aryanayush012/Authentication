1b12cc15cccfef155b0f96af07433bb14e0e7b88
Ayush Aryan <aryanayush012@gmail.com>
2022-09-08 01:01:02 +0530

    google Oauth security

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const mongoose = require("mongoose");
 //const md5 = require("md5");
 // const bcrypt = require("bcrypt");
 // const saltRounds =10;
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require("mongoose-findorcreate");
 
 const session = require('express-session');
 const passport = require("passport");
@@ -54,7 +56,8 @@ const userSchema= new mongoose.Schema({
     },
     phone:{
         type:String
-    }
+    },
+    googleId:String
 
 });
 
@@ -62,17 +65,51 @@ const userSchema= new mongoose.Schema({
 //userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:["password"]});
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 const User = new mongoose.model("User",userSchema);
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+// passport.serializeUser(User.serializeUser());
+// passport.deserializeUser(User.deserializeUser());
+
+passport.serializeUser(function(user,done){
+    done(null,user.id); 
+});
+
+passport.deserializeUser(function(id,done){
+    User.findById(id,function(err,user){
+        done(err,user);
+    });
+});
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/profile",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile);
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 
 app.get("/",function(req,res){
     res.render("home");
 });
+
+app.get("/auth/google",
+  passport.authenticate("google", { scope: ["profile"] }));
+
+  app.get("/auth/google/profile", 
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect("/profile");
+  });
 
 app.get("/login",function(req,res){
     res.render("login");
